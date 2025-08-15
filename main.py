b4bf05d7a17bfda3d2689a31c7f1647a1ebda6cf
iremeroglu27 <67irem81@gmail.com>
2025-08-15 20:10:40 +0300

    add list comprehension

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ while len(correct_guesses) < 50:
     answer_state = screen.textinput(title=f" {count}/50 Guess The State", prompt="What's another states name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in correct_guesses:
-                missing_states.append(state)
+        
+        missing_states = [state for state in all_states if state not in correct_guesses]
+        
         new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("./day 25 CsvFiles/U.S.-States-Game/states_to_learn.csv")
+        
+        new_data.to_csv("./day 26 ListComprehension/U.S. States Game Update/states_to_learn.csv")
 
         break
     if answer_state in all_states and answer_state not in correct_guesses:
@@ -43,3 +43,4 @@ while len(correct_guesses) < 50:
         t.goto(position)
         t.write(f"{answer_state}", align="center", font=FONT)      
         
+    
